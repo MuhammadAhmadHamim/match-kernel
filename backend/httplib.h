d03296661010219f4c98dbe7d77d4a2f4767fb55
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-05-08 01:55:53 +0500

    Added the header file which would help us create a HTTP server in Cpp

--- backend/httplib.h
+++ backend/httplib.h
@@ -13,7 +13,7 @@
 
 #ifdef _WIN32
 #if defined(_WIN32_WINNT) && _WIN32_WINNT < 0x0A00
-#error                                                                         \
+#error
     "cpp-httplib doesn't support Windows 8 or lower. Please use Windows 10 or later."
 #endif
 #endif
