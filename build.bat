3bf5cb8b526c329a03c3b9511d1d0793af3af0e1
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-05-16 19:40:25 +0500

    Configured the build.bat to work as a Magnent link

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 @echo off
 g++ backend/main.cpp -std=c++17 -o server -D_WIN32_WINNT=0x0A00 -lws2_32 -lwininet -static-libgcc -static-libstdc++
 echo Successfully built!
+echo Starting MatchKernel server...
+start server.exe
+timeout /t 2 /noisy >nul
+start "" "frontend/index.html"
